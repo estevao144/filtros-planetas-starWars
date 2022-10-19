d33f0020477651efd7cd78b9b5cca42a51ea05b2
Estevao <estevo144@gmail.com>
2022-10-19 18:42:45 -0300

    questao 2 ok1

--- src/components/SearchName.jsx
+++ src/components/SearchName.jsx
@@ -4,6 +4,7 @@ import myContext from '../context/MyContext';
 function SearchName() {
   const { handleSearchChange } = useContext(myContext);
   return (
+
     <div>
       <h1> Star Wars Busca: </h1>
       <input
@@ -12,6 +13,52 @@ function SearchName() {
         type="text"
         onChange={ ({ target }) => handleSearchChange(target.value) }
       />
+      {/* <form>
+        <select
+          id="column"
+          data-testid="column-filter"
+          onChange={ ({ target }) => (
+            handleFilterChange(target.id, target.value)
+          ) }
+        >
+          {
+            columns.map((column) => (
+              <option
+                key={ column }
+              >
+                { column }
+              </option>
+            ))
+          }
+        </select>
+        <select
+          id="comparison"
+          data-testid="comparison-filter"
+          onChange={ ({ target }) => (
+            handleFilterChange(target.id, target.value)
+          ) }
+        >
+          <option>maior que</option>
+          <option>menor que</option>
+          <option>igual a</option>
+        </select>
+        <input
+          id="value"
+          type="number"
+          data-testid="value-filter"
+          value={ filters.filterByNumericValues.value }
+          onChange={ ({ target }) => (
+            handleFilterChange(target.id, target.value)
+          ) }
+        />
+        <button
+          data-testid="button-filter"
+          type="button"
+          onClick={ handleFilterClick }
+        >
+          Filtrar
+        </button>
+      </form> */}
     </div>
   );
 }
